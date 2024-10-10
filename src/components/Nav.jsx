46b0ffefabfa1f34005329ed874bc1ee7e4e6983
CCT <icaretw2016@gmail.com>
2024-10-10 23:35:32 +0800

    fix: Ugly fix to tightly coupled to gh-page home page link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import srcCode from "./img/github-logo.png";
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/#">
+      <a className="navbar-brand" href="/housing">
         CareTw
       </a>
       <button
@@ -21,7 +21,7 @@ const Nav = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link disabled" href="/">
+            <a className="nav-link disabled" href="/housing">
               社會住宅
             </a>
           </li>
