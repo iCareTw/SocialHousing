957f4c706432bfd0ef8ddecd0104f90a528e16e4
OMG <icaretw2016@gmail.com>
2025-11-07 10:31:30 +0800

    impl: add 202510

--- src/components/SocialHousing/dataSources.jsx
+++ src/components/SocialHousing/dataSources.jsx
@@ -1,5 +1,10 @@
 export const dataSources = [
   {
+    ym: "202510",
+    source:
+      "https://pip.moi.gov.tw/Upload/File/SocialHousing/(上網版)1141031-社會住宅興辦案件執行情形彙整表.pdf"
+  },
+  {
     ym: "202509",
     source:
       "https://pip.moi.gov.tw/Upload/File/SocialHousing/(上網版)1140930-社會住宅興辦案件執行情形彙整表.pdf"
