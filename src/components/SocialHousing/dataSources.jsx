5f0e43b4fbb85de4f03def3adb7f0e6ca4ac50f7
Tida <icaretw2016@gmail.com>
2026-03-06 00:08:43 +0800

    impl: 202602

--- src/components/SocialHousing/dataSources.jsx
+++ src/components/SocialHousing/dataSources.jsx
@@ -1,5 +1,10 @@
 export const dataSources = [
   {
+    ym: "202602",
+    source:
+      "https://pip.moi.gov.tw/Upload/File/SocialHousing/1150228-社會住宅興辦案件執行情形彙整表.pdf"
+  },
+  {
     ym: "202601",
     source:
       "https://pip.moi.gov.tw/Upload/File/SocialHousing/(上網版)1150131-社會住宅興辦案件執行情形彙整表v2.pdf"
