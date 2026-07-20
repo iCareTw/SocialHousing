c90043189952845c54ce1f84a94af161ebcc6126
OMG <icaretw2016@gmail.com>
2026-07-20 10:35:03 +0800

    impl: 202606

--- src/components/SocialHousing/dataSources.jsx
+++ src/components/SocialHousing/dataSources.jsx
@@ -1,5 +1,10 @@
 export const dataSources = [
   {
+    ym: "202606",
+    source:
+      "https://pip.moi.gov.tw/Upload/File/SocialHousing/(上網版)1150630-社會住宅興辦案件執行情形彙整表v2.pdf"
+  },
+  {
     ym: "202605",
     source:
       "https://pip.moi.gov.tw/Upload/File/SocialHousing/(上網版)1150531-社會住宅興辦案件執行情形彙整表v2.pdf"
